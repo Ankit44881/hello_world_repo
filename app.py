bf1825867a31a14b24470ee3fa220e0fdbecf80b
Ankit Raj <ankitraj84097@gmail.com>
2024-10-05 15:38:43 +0000

    increase size of text

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Get well Soon Nikku!'
+    return '<h1 style="font-size: 48px;">Get well Soon Nikku!</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
