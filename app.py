25cb56f6ef4ddfb3d4e43ad02ff7d74fef4d9037
Ankit Raj <ankitraj84097@gmail.com>
2024-10-06 16:48:33 +0000

    updated build file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geeks():
-    return "Hello Geeks from Ankita Rai!"
+    return "Hello Geeks from Ankit Ankita!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
