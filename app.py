92d065a66789b7b3d7bf14f2baf54970e9c0e98c
Ankit44881 <ankitraj84097@gmail.com>
2024-10-06 20:17:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,26 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '''
-    <html>
-        <head>
-            <style>
-                body {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                    margin: 0;
-                    font-size: 10vw; /* Responsive font size */
-                }
-            </style>
-        </head>
-        <body>
-            <h1>Hi Everyone!</h1>
-        </body>
-    </html>
-    '''
+def hello_geeks():
+    return "Hello Geeks from Ankit!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
